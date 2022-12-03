3dabdc3892686947a0e477cb80343b62f03b7990
allann15 <allanarruda19@gmail.com>
2022-12-02 21:49:55 -0300

    Primeiro Projeto Angular

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,28 @@
+export const dataFake = [
+{
+    "id": "1",
+    "photoCover": "https://s2.glbimg.com/T5o9xSEPTTjUb3OdGRkT6Qtioik=/0x60:5662x3245/810x456/smart/filters:max_age(3600)/https://i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2022/5/6/8bpwm1TyORu1QEiVJ3LA/2022-12-02t190348z-2128966096-up1eic21gyajg-rtrmadp-3-soccer-worldcup-cmr-bra-report.jpg",
+    "title": "Brasil Perdeu",
+    "description": "Brasil perde seu primeiro jogo na copa do mundo, a derrota aconteceu pelo camarões com o resultado de 1x0, gol de Aboubakar"
+}, 
+
+{
+    "id": "2",
+    "photoCover": "https://observatoriodocinema.uol.com.br/wp-content/uploads/2022/08/wandinha-netflix.jpg",
+    "title": "Wandinha nova série da Netflix",
+    "description": "Inteligente, sarcástica e apática, Wandinha Addams pode estar meio morta por dentro, mas na Escola Nunca Mais ela vai fazer amigos, inimigos e investigar assassinatos. Assista o quanto quiser. Tim Burton dirige esta série sombria e divertida estrelada por Jenna Ortega"
+},
+{
+    "id": "3",
+    "photoCover": "https://s2.glbimg.com/tL4picUYzadvPFzu3hjskBXlLUA=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2022/p/o/nk8Ar7QuC2NgmSeXxaVg/pantera-negra-wakanda-forever-para-sempre-sinopse-elenco-trailer-filme.jpg",
+    "title": "Novo filme Pantera Negra Já Disponível",
+    "description": "Rainha Ramonda, Shuri, M'Baku, Okoye e Dora Milaje lutam para proteger sua nação das potências mundiais intervenientes após a morte do rei T'Challa."
+},
+{
+    "id": "4",
+    "photoCover": "https://poltronavip.com/wp-content/uploads/2021/01/guardioes-da-galaxia-vol-2-chris-pratt.jpg",
+    "title": "Novo filme Guardiões da galáxia 3, disponível em breve",
+    "description": "Encerramento da trilogia estrelada por Peter Quill / Star-Lord (Chris Pratt), Gamora (Zoe Saldana), Rocket (Bradley Cooper), Drax (Dave Bautista) e Groot (Vin Diesel). A sinopse oficial ainda não foi divulgada."
+}
+]
+
